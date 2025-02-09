86e4abf37f38390a5a641efcd6d065e9ed00d90d
yogaraj.s <yogarajsivaprakasam@gmail.com>
2025-02-09 20:20:21 +0530

    add: google adsense
    Signed-off-by: yogaraj.s <yogarajsivaprakasam@gmail.com>

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,7 +10,8 @@ export default defineUserConfig({
   bundler: viteBundler(),
   theme,
   head:[
-    ['meta', {name: 'google-site-verification', 'content': 'fiql57E-4MbcN3dvCE3NYp2e5vRu03Pg2bDERier0gc'}]
+    ['meta', {name: 'google-site-verification', 'content': 'fiql57E-4MbcN3dvCE3NYp2e5vRu03Pg2bDERier0gc'}],
+    ['meta', {name: 'google-adsense-account', 'content': 'ca-pub-8634712203742253'}],
   ],
   lang: "en-US",
   title: "Yogaraj.S",
